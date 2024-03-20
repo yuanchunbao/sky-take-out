6dbdb504cfbb0f8c7b8c5d34d743eb8038cafb1f
yuanchunbao <yuanchunbao@emails.bjut.edu.cn>
2024-03-20 17:03:19 +0800

    删除菜品相关业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,4 +10,12 @@ import java.util.List;
 @Mapper
 public interface DishFlavorMapper {
     void insertBatch(List<DishFlavor> flavors);
+
+
+    /**
+     * 根据菜品id删除对应的口味数据
+     * @param DishId
+     */
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long DishId);
 }
--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,9 +1,13 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -30,4 +34,16 @@ public interface DishMapper {
     void insert(Dish dish);
 
 
+    Page<DishVO> pageQurey(DishPageQueryDTO dishPageQueryDTO);
+
+
+    @Select("select * from dish where id = #{id}")
+    Dish getById(Long id);
+
+    /**
+     * 根据主键删除数据
+     * @param id
+     */
+    @Delete("delete from dish where id = #{id}")
+    void deleteByIds(Long id);
 }
